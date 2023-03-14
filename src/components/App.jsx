838f98092b509fb89dd896e65667c49fb041677c
Mifa79 <anh.tx21@gmail.com>
2023-03-14 16:54:22 -0500

    add ProductCard component with image changed on mouse hover event

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,29 @@
 import '../App.css';
 import Navbar from './Navbar'
 import HeroBanner from './HeroBanner';
+import ProductCard from './ProductCard';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <HeroBanner></HeroBanner>
-     
+      <div className="popular-pick">
+        <div className="row">
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+        </div>
+        <div className="row">
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+        </div>
+      </div>
+
+
     </div>
   );
 }
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,38 @@
+import React, { useRef } from "react";
+import ProductImage from "../assets/images/product_card.jpeg";
+import ProductImageHover from "../assets/images/product_card_hover.jpg";
+
+const ImageToggleOnMouseOver = ({ primaryImg, secondaryImg }) => {
+    const imageRef = useRef(null);
+    return (
+        <img
+            className="product-image"
+            onMouseOver={() => { imageRef.current.src = secondaryImg; }}
+            onMouseOut={() => { imageRef.current.src = primaryImg; }}
+            src={ProductImage}
+            alt="product"
+            ref={imageRef}
+        />
+    )
+}
+
+const ImageChangeOnMouseOver = () => {
+    return (
+        <div>
+            <ImageToggleOnMouseOver
+                primaryImg={ProductImage}
+                secondaryImg={ProductImageHover}
+                alt="" />
+        </div>
+    )
+}
+
+function ProductCard() {
+    return (
+        <div className="product-item col-lg-3 col-md-4 col-sm-6">
+            <ImageChangeOnMouseOver />
+        </div>
+    );
+}
+
+export default ProductCard;
