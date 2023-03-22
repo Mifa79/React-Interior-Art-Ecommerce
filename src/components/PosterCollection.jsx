24b0dbcb58564af12b5af89e85c6758ffe7b5592
Mifa79 <anh.tx21@gmail.com>
2023-03-22 15:18:27 -0500

    use filter function to display only products from Poster Collection on the corresponding page

--- src/components/PosterCollection.jsx
+++ src/components/PosterCollection.jsx
@@ -1,22 +1,30 @@
 import React from "react";
-import ProductCard from "./ProductCard";
+import ProductCard from './ProductCard';
+import products from "../products";
+
+function createProductCard(product) {
+    return (
+        <ProductCard
+            key={product.id}
+            name={product.name}
+            artist={product.artist}
+            imgURL={product.imgURL}
+            imgHoverURL={product.imgHoverURL}
+            pricing={product.pricing}
+            collection={product.collection}
+        />
+    );
+}
 
 function PosterCollection() {
+    const posterProducts = products.filter(product => product.collection === "Poster Collection");
     return (
-        <div>
+        <div className="poster collection">
             <h2>Poster Collection</h2>
             <div className="row">
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
+                {posterProducts.map(createProductCard)}
             </div>
-            <div className="row">
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
-            </div>
+            
         </div>
     );
 }
