57f7b1cef1460de82dc65cdfa1601a47d4a32d0d
Mifa79 <anh.tx21@gmail.com>
2023-03-22 15:15:24 -0500

    use filter function to render only products from the Signature Collection on the corresponding page

--- src/components/SignatureCollection.jsx
+++ src/components/SignatureCollection.jsx
@@ -1,22 +1,30 @@
 import React from "react";
-import ProductCard from "./ProductCard";
+import ProductCard from './ProductCard';
+import products from "../products";
+
+function createProductCard(product) {
+    return (
+        <ProductCard
+            key={product.id}
+            name={product.name}
+            artist={product.artist}
+            imgURL={product.imgURL}
+            imgHoverURL={product.imgHoverURL}
+            pricing={product.pricing}
+            collection={product.collection}
+        />
+    );
+}
 
 function SignatureCollection() {
+    const signatureProducts = products.filter(product => product.collection === "Signature Collection");
     return (
-        <div>
+        <div className="all-products">
             <h2>Signature Collection</h2>
             <div className="row">
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
+                {signatureProducts.map(createProductCard)}
             </div>
-            <div className="row">
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
-            </div>
+            
         </div>
     );
 }
