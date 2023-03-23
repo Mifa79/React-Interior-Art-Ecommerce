9afc3a20668720ac0a6436eb3d4771b744dadfc8
Mifa79 <anh.tx21@gmail.com>
2023-03-23 18:15:41 -0500

    add ProductSelectionToCart as a separate component

--- src/components/ProductDetailsInfos.jsx
+++ src/components/ProductDetailsInfos.jsx
@@ -1,21 +1,13 @@
 import React from "react";
-import ImageSizeSelection from "./ImageSizeSelection";
-import IncreaseDecreaseQuantityButton from "./IncreaseDecreaseQuantityButton";
 import ProductDetailsHeader from "./ProductDetailsHeader";
+import ProductSelectionToCart from "./ProductSelectionToCart";
 
 function ProductDetailsInfos() {
     return (
         <div className="col-4 product-infos">
             <ProductDetailsHeader></ProductDetailsHeader>
             <div class="border-product"></div>
-            <h5 className="select-image-size-title">Select image size:</h5>
-            <ImageSizeSelection></ImageSizeSelection>
-            <h4 className="pricing-value">$25</h4>
-            <h5 className="select-image-size-title">Select quantity:</h5>
-            <IncreaseDecreaseQuantityButton></IncreaseDecreaseQuantityButton>
-            <div className="add-to-cart-with-quantity">
-                <button class="btn btn-primary add-to-cart-button" type="button">ADD TO CART</button>
-            </div>
+            <ProductSelectionToCart></ProductSelectionToCart>
             <div class="border-product"></div>
             <h5>Product Description</h5>
             <ul className="product-description">
---
+++ src/components/ProductSelectionToCart.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ImageSizeSelection from "./ImageSizeSelection";
+import IncreaseDecreaseQuantityButton from "./IncreaseDecreaseQuantityButton";
+
+function ProductSelectionToCart() {
+    return (
+        <div>
+            <h5 className="select-image-size-title">Select image size:</h5>
+            <ImageSizeSelection></ImageSizeSelection>
+            <h4 className="pricing-value">$25</h4>
+            <h5 className="select-image-size-title">Select quantity:</h5>
+            <IncreaseDecreaseQuantityButton></IncreaseDecreaseQuantityButton>
+            <div className="add-to-cart-with-quantity">
+                <button class="btn btn-primary add-to-cart-button" type="button">ADD TO CART</button>
+            </div>
+        </div>
+    );
+}
+
+export default ProductSelectionToCart;
