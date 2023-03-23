aeb0bb7969da6c17fde6fb47c0ecc8b5623a2863
Mifa79 <anh.tx21@gmail.com>
2023-03-23 16:32:22 -0500

    adjust layout and refine styling for ProductDetails page

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -29,20 +29,26 @@ function ProductDetails() {
                 </div>
 
             </div>
-            <div className="col-6 product-infos">
-                <h3 className="product-name-artist">Cafe Terrace at Night - Vincent Van Gogh</h3>
-                <h4 className="pricing-value">$25</h4>
+            <div className="col-4 product-infos">
+                <h3 className="product-name">Cafe Terrace at Night</h3>
+                <p className="product-collection"><i class="fa-sharp fa-solid fa-barcode"></i>   Signature Collection</p>
+                <p className="product-artist"><i class="fa-solid fa-palette"></i>   Vincent Van Gogh</p>
+                
+                <div class="border-product"></div>
+                
                 <h5 className="select-image-size-title">Select image size:</h5>
                 <ImageSizeSelection></ImageSizeSelection>
+                <h4 className="pricing-value">$25</h4>
                 <h5 className="select-image-size-title">Select quantity:</h5>
                 <IncreaseDecreaseQuantityButton></IncreaseDecreaseQuantityButton>
                 <div className="add-to-cart-with-quantity">
                     
                     <button class="btn btn-primary add-to-cart-button" type="button">ADD TO CART</button>
                 </div>
+                <div class="border-product"></div>
                 <h5>Product Description</h5>
                 <ul className="product-description">
-                    <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                    <li>Lorem ipsum dolor sit amet, consectetuer adipiscing.</li>
                     <li>Aliquam tincidunt mauris eu risus.</li>
                     <li>Vestibulum auctor dapibus neque.</li>
                     <li>Nunc dignissim risus id metus.</li>
