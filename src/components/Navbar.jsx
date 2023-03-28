9495b4854f14beff651f94deef977c83df48bf53
Mifa79 <anh.tx21@gmail.com>
2023-03-28 16:27:45 -0500

    add Cart item on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,10 +34,17 @@ function Navbar() {
                             <a className="nav-link disabled">Disabled</a>
                         </li> */}
                     </ul>
-                    <form className="d-flex">
+
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link">CART(0)</a>
+                        </li>
+                    </ul>
+
+                    {/* <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                    </form> */}
                 </div>
             </div>
         </nav>
