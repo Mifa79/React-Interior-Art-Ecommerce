a454958c1bef37c34a3ed3453a0fdfa2f4078c29
Mifa79 <anh.tx21@gmail.com>
2023-03-29 14:38:42 -0500

    make product item quantity increase and decrease on button click

--- src/components/IncreaseDecreaseQuantityButton.jsx
+++ src/components/IncreaseDecreaseQuantityButton.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function IncreaseDecreaseQuantityButton() {
+    const [count, setCount] = useState(0);
+
+    function increase() {
+        setCount (count + 1);
+    }
+
+    function decrease() {
+        if (count > 0) {
+            setCount (count - 1);
+        } else {
+            setCount(0);
+        }
+    }
+
     return (
         <div class="d-flex">
             <div class="input-group">
-                <input type="button" value="-" class="button-minus border rounded-circle  icon-shape icon-sm" data-field="quantity" />
-                <p className="quantity-field"><span className="quantity-number">1</span></p>
-                {/* <input type="number" step="1" max="10" value="1" name="quantity" class="quantity-field" /> */}
-                <input type="button" value="+" class="button-plus border rounded-circle icon-shape icon-sm " data-field="quantity" />
+                <button class="button-minus border rounded-circle  icon-shape icon-sm" onClick={decrease}>-</button>
+                <p className="quantity-field"><span className="quantity-number">{count}</span></p>
+                <button class="button-minus border rounded-circle  icon-shape icon-sm" onClick={increase}>+</button>
             </div>
         </div>
     );
