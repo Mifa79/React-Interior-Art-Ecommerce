304644f5e613d71a19570c6e3fb306be3ce441af
Mifa79 <anh.tx21@gmail.com>
2023-03-30 15:02:04 -0500

    add product image, name, poster size, and pricing into cart table

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ThinHorizontalLine from "./ThinHorizontalLine";
+import product1Image from '../assets/images/product_1.webp';
+import product1HoverImage from '../assets/images/product_1_hover.webp';
 
 function Cart() {
     return (
@@ -12,6 +14,25 @@ function Cart() {
                     <div className="col-3 cart-table-heading-total">Total</div>
                 </div>
                 <ThinHorizontalLine></ThinHorizontalLine>
+                <div className="row cart-table-content">
+                    <div className="col-6">
+                        <div className="row cart-table-product-info-container">
+                            <img src={product1HoverImage} className="cart-table-product-image"></img>
+                            <div className="cart-table-product-info">
+                                <p className="cart-table-product-name">Cafe Terrace at Night</p>
+                                <p className="cart-table-product-size">Poster Size: 18" x 24"</p>
+                                <p className="cart-table-product-pricing">$25</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3 cart-adjust-quantity-button">
+
+                    </div>
+                    <div className="col-3 cart-pricing">
+
+                    </div>
+
+                </div>
             </div>
 
 
