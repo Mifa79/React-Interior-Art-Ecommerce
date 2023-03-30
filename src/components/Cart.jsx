9351f9c6d156a48ba2b20bcc433b959e36435824
Mifa79 <anh.tx21@gmail.com>
2023-03-30 15:33:27 -0500

    create CartTableRow component which allows adjust item quantity and display total pricing

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ThinHorizontalLine from "./ThinHorizontalLine";
-import product1Image from '../assets/images/product_1.webp';
-import product1HoverImage from '../assets/images/product_1_hover.webp';
+import CartTableRow from "./CartTableRow";
 
 function Cart() {
     return (
@@ -9,34 +8,14 @@ function Cart() {
             <h2>Shopping Cart</h2>
             <div className="cart-table">
                 <div className="row cart-table-heading">
-                    <div className="col-6 cart-table-heading-product">Product</div>
-                    <div className="col-3 cart-table-heading-quantity">Quantity</div>
-                    <div className="col-3 cart-table-heading-total">Total</div>
+                    <div className="col-8 cart-table-heading-product">Product</div>
+                    <div className="col-2 cart-table-heading-quantity">Quantity</div>
+                    <div className="col-2 cart-table-heading-total">Total</div>
                 </div>
                 <ThinHorizontalLine></ThinHorizontalLine>
-                <div className="row cart-table-content">
-                    <div className="col-6">
-                        <div className="row cart-table-product-info-container">
-                            <img src={product1HoverImage} className="cart-table-product-image"></img>
-                            <div className="cart-table-product-info">
-                                <p className="cart-table-product-name">Cafe Terrace at Night</p>
-                                <p className="cart-table-product-size">Poster Size: 18" x 24"</p>
-                                <p className="cart-table-product-pricing">$25</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-3 cart-adjust-quantity-button">
-
-                    </div>
-                    <div className="col-3 cart-pricing">
-
-                    </div>
-
-                </div>
+                <CartTableRow></CartTableRow>
+                <CartTableRow></CartTableRow>
             </div>
-
-
-            
         </div>
     );
 }
---
+++ src/components/CartTableRow.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ThinHorizontalLine from "./ThinHorizontalLine";
+import IncreaseDecreaseQuantityButton from "./IncreaseDecreaseQuantityButton";
+import product1Image from '../assets/images/product_1.webp';
+import product1HoverImage from '../assets/images/product_1_hover.webp';
+
+function CartTableRow() {
+    return (
+        <div>
+            <div className="row cart-table-row">
+                <div className="col-8">
+                    <div className="row cart-table-product-info-container">
+                        <img src={product1HoverImage} className="cart-table-product-image"></img>
+                        <div className="cart-table-product-info">
+                            <p className="cart-table-product-name">Cafe Terrace at Night</p>
+                            <p className="cart-table-product-size">Poster Size: 18" x 24"</p>
+                            <p className="cart-table-product-pricing">$25</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-2 cart-adjust-quantity-button">
+                    <IncreaseDecreaseQuantityButton></IncreaseDecreaseQuantityButton>
+                </div>
+                <p className="col-2 cart-item-total-pricing">$25</p>
+            </div>
+            <ThinHorizontalLine></ThinHorizontalLine>
+        </div>
+    );
+}
+
+export default CartTableRow;
