498a027403e46ce1e53882a590a6c9d190f72e5e
Mifa79 <anh.tx21@gmail.com>
2023-04-03 21:56:30 -0500

    update the styling to create a responsive design for the ProductCategory section

--- src/components/ProductCategory.jsx
+++ src/components/ProductCategory.jsx
@@ -3,21 +3,20 @@ import CategoryImage from "../assets/images/category_1.jpeg";
 
 function ProductCategory() {
     return (
-        <div className="product-category row">
-            <div className="category-image-container col-lg-6 col-md-6">
+        <div className="product-category">
+            <div className="category-image-container">
                 <img className="category-image" src={CategoryImage} />
             </div>
-            <div className="category-info-container col-lg-6 col-md-6">
+            <div className="category-info-container">
                 <div className="category-info">
-                    <h2>Signature Collection</h2>
-                    <p className="category-description">Discover the Signature Collection, featuring a curated selection of high-quality prints from top artists around the world. These iconic pieces are sure to make a statement in any room and are perfect for those seeking unique, one-of-a-kind artwork.</p>
-                    <button class="btn btn-primary shop-by-categories-button" type="button">SHOP NOW</button>
+                    <div className="category-info-inner">
+                        <h2>Signature Collection</h2>
+                        <p className="category-description">Discover the Signature Collection, featuring a curated selection of high-quality prints from top artists around the world. These iconic pieces are sure to make a statement in any room and are perfect for those seeking unique, one-of-a-kind artwork.</p>
+                        <button class="btn btn-primary shop-by-categories-button" type="button">SHOP NOW</button>
+                    </div>
                 </div>
-
             </div>
-
         </div>
-
     );
 }
 
