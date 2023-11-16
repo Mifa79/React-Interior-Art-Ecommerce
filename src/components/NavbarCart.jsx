4b9e9283cf5965c47253ed13a754e7db9164526c
Mifa79 <anh.tx21@gmail.com>
2023-11-16 13:49:58 -0600

    use reducer and context to calculate total item quantity added in cart and display it on the Navbar

--- src/components/NavbarCart.jsx
+++ src/components/NavbarCart.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useCart } from '../CartContext';
 
 function NavbarCart() {
+    const cartItems = useCart();
+
+    const calculateTotalQuantity = (cartItems) => {
+        return cartItems.reduce((total, item) => {
+            return total + item.quantity;
+        }, 0);
+    };
+
     return (
         <li className="nav-item">
-            <a className="nav-link" href="/cart">CART(0)</a>
+            <a className="nav-link" href="/cart">CART({calculateTotalQuantity(cartItems)})</a>
         </li>
     );
 }
