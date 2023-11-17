686222077947e651d631b0bfc3d786641c352ef5
Mifa79 <anh.tx21@gmail.com>
2023-11-17 17:09:43 -0600

    refine styling for Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,60 +3,52 @@ import React from "react";
 function Footer() {
     return (
         <div className="footer">
-            <footer className="text-center text-lg-start bg-light text-muted">
 
-                <section className="footer-info-container">
+            <section className="row footer-info-container">
+                    <div className="footer-nav">
+                        <h5>HOME</h5>
+                        <h5>ABOUT US</h5>
+                        <h5>ALL PRODUCTS</h5>
+                    </div>
 
-                    <div className="row footer-info mt-3">
-
-                        <div className="footer-nav">
-                            <h5>HOME</h5>
-                            <h5>ABOUT US</h5>
-                            <h5>ALL PRODUCTS</h5>
-                        </div>
-
-                        <div className="company-info">
-                            <p className="logo-footer">Budding Joy Studio</p>
-                            <p className="company-brief">
+                    <div className="company-info">
+                        <p className="logo-footer">Budding Joy Studio</p>
+                        <p className="company-brief">
                             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum
-                                dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum
-                                dolor sit amet, consectetur adipisicing elit.
-                            </p>
-                        </div>
-
+                            dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum
+                            dolor sit amet, consectetur adipisicing elit.
+                        </p>
                     </div>
+            </section>
 
-                </section>
-
-                <section className="row copyright-social-media">
-                    <div className="copyright-container">
-                        <p className="copyright">© 2023 Budding Joy Studio. All rights reserved.</p>
-                    </div>
-                    <div className="social-media-container">
-                        <a href="#" className="me-4 text-reset">
-                            <i className="fab fa-facebook-f"></i>
-                        </a>
-                        <a href="#" className="me-4 text-reset">
-                            <i className="fab fa-twitter"></i>
-                        </a>
-                        <a href="#" className="me-4 text-reset">
-                            <i className="fab fa-google"></i>
-                        </a>
-                        <a href="#" className="me-4 text-reset">
-                            <i className="fab fa-instagram"></i>
-                        </a>
-                        <a href="#" className="me-4 text-reset">
-                            <i className="fab fa-linkedin"></i>
-                        </a>
-                        <a href="#" className="me-4 text-reset">
-                            <i className="fab fa-github"></i>
-                        </a>
-                    </div>
-                </section>
-
-            </footer>
+            <section className="row copyright-social-media">
+                <div className="copyright-container">
+                    <p className="copyright">© 2023 Budding Joy Studio. All rights reserved.</p>
+                </div>
+                <div className="social-media-container">
+                    <a href="#" className="me-4 text-reset">
+                        <i className="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#" className="me-4 text-reset">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href="#" className="me-4 text-reset">
+                        <i className="fab fa-google"></i>
+                    </a>
+                    <a href="#" className="me-4 text-reset">
+                        <i className="fab fa-instagram"></i>
+                    </a>
+                    <a href="#" className="me-4 text-reset">
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                    <a href="#" className="me-4 text-reset">
+                        <i className="fab fa-github"></i>
+                    </a>
+                </div>
+            </section>
 
         </div>
+
     );
 }
 
