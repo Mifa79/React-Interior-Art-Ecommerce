184583c9ce8ffbeee1bff2bc3a19210c38e2491d
Mifa79 <anh.tx21@gmail.com>
2023-11-17 23:55:21 -0600

    fix image carousel next and prev button in Product Details page

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -5,7 +5,7 @@ import ThinHorizontalLine from "./ThinHorizontalLine";
 
 function HeroBanner() {
     return (
-        <div id="carouselExampleControls" className="carousel-slide" data-bs-ride="carousel">
+        <div>
             <div className="row hero-banner">
                 <div className="hero-text-banner">
                     <div className="hero-text-container">
@@ -19,26 +19,6 @@ function HeroBanner() {
                     <img className="hero-image" src={HeroImage} />
                 </div>
             </div>
-            
-            {/* <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src={HeroImageFull} className="d-block w-100" alt="..." />
-                </div>
-                <div className="carousel-item">
-                    <img src={HeroImageFull} className="d-block w-100" alt="..." />
-                </div>
-                <div className="carousel-item">
-                    <img src={HeroImageFull} className="d-block w-100" alt="..." />
-                </div>
-            </div> */}
-            {/* <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button> */}
         </div>
 
     );
--- src/components/ProductImageCarousel.jsx
+++ src/components/ProductImageCarousel.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import ProductImage from "../assets/images/product_1.webp";
-import InRoomImage from "../assets/images/product_1_hover.webp";
 
 function ProductImageCarousel(props) {
-
     return (
-        <div id="carouselExampleControls" className="carousel-slide" data-bs-ride="carousel">
+        <div id={`carousel-${props.id}`} className="product-carousel-slide carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
                 <div className="carousel-item active">
                     <img src={props.imgURL} className="d-block w-100" alt="..." />
@@ -13,23 +10,15 @@ function ProductImageCarousel(props) {
                 <div className="carousel-item">
                     <img src={props.imgHoverURL} className="d-block w-100" alt="..." />
                 </div>
-                <div className="carousel-item">
-                    <img src={props.imgHoverURL} className="d-block w-100" alt="..." />
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <div className="carousel-control-circle">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </div>
-
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <div className="carousel-control-circle">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </div>
-                </button>
             </div>
+            <button className="carousel-control-prev carousel-control-circle" type="button" data-bs-target={`#carousel-${props.id}`} data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next carousel-control-circle" type="button" data-bs-target={`#carousel-${props.id}`} data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
         </div>
     );
 }
