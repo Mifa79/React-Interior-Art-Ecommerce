a3414e883102f955068fc48d38611db16d4fa564
Mifa79 <anh.tx21@gmail.com>
2023-11-22 07:55:34 -0600

    fix loading new page at the bottom error by using scrollTo(0,0)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,9 @@ import ContemporaryCollection from "./ContemporaryCollection";
 import HeritageCollection from "./HeritageCollection";
 import ProductDetails from "./ProductDetails";
 import Footer from './Footer';
-
+import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import {
   BrowserRouter as Router,
   Routes,
@@ -20,29 +22,41 @@ import {
   Navigate,
 } from "react-router-dom";
 
+function ScrollToTop() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   const [cart, setCart] = useState([]);
 
   return (
     <div className="App">
-    <Router>
-      <Navbar></Navbar>
-      <Routes className="body">
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/allproducts" element={<AllProducts />} />
-        <Route path="/contemporaryCollection" element={<ContemporaryCollection />} />
-        <Route path="/heritagecollection" element={<HeritageCollection />} />
-        <Route path="/productdetails/:productID" element={<ProductDetails />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-      <Footer></Footer>
-    </Router>
-  </div>
+      <Router>
+        <ScrollToTop />
+        <Navbar></Navbar>
+        <Routes className="body">
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/allproducts" element={<AllProducts />} />
+          <Route path="/contemporaryCollection" element={<ContemporaryCollection />} />
+          <Route path="/heritagecollection" element={<HeritageCollection />} />
+          <Route path="/productdetails/:productID" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+        <Footer></Footer>
+      </Router>
+    </div>
 
 
 
-    
+
   );
 }
 
